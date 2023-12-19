364b65c2120734d8b65596ba3fd9af4bcfc3a20d
tv0823 <tv0823@bs.amalnet.k12.il>
2023-12-19 09:51:22 +0200

    final

--- app/src/main/java/com/example/textview_ex4/MainActivity.java
+++ app/src/main/java/com/example/textview_ex4/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.textview_ex4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tV;
+    int timeClicked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+    public void pressed(View view) {
+        timeClicked ++;
+        tV.setTextColor(Color.YELLOW);
+        if (timeClicked % 7 == 0)
+        {
+            tV.setText("BOOM");
+        }
+        else
+        {
+            tV.setText("This is a click number: "+timeClicked);
+        }
     }
 }
